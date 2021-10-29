4239cdadb0fe60b4ca27ba45508670a7f076a741
TechJohnson <steven@techjohnson.com>
2021-10-28 23:28:02 -0400

    Update navigation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+// import App from "./App";
 import Roadmap from "./roadmap";
 import Discord from "./discord";
 import reportWebVitals from "./reportWebVitals";
